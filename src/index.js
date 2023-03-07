6f98b3391e28b82ea418f813af9fe08302ab87a6
aarsaevv <arsaevgeny@gmail.com>
2023-03-07 14:26:47 +0600

    loading, stars, names as links

--- src/index.js
+++ src/index.js
@@ -31,8 +31,10 @@ function validateAndSubmit(e, searchText) {
 }
 
 async function fetchRepos(text) {
+    showLoadingMessage()
     await fetch(`https://api.github.com/search/repositories?q=${text}&page=1&per_page=10`)
     .then(res => res.json()).then(json => {
+        hideLoadingMessage()
         if (json.total_count == 0) {
             showUnsuccessMessage()
         }
@@ -43,7 +45,7 @@ async function fetchRepos(text) {
 function showUnsuccessMessage() {
     let resultList = document.querySelector('.result')
     let unsuccessMessage = document.createElement('li')
-    unsuccessMessage.classList.add('result__item')
+    unsuccessMessage.classList.add('result__unsuccess')
     unsuccessMessage.textContent = 'Ничего не найдено'
     resultList.append(unsuccessMessage)
 }
@@ -55,22 +57,31 @@ function appendResultsToPage(array) {
         let li = document.createElement('li')
         li.classList.add('result__item')
 
-        let name = document.createElement('div')
+        let repoLink = document.createElement('a')
         let description = document.createElement('div')
         let owner = document.createElement('div')
+        let stars = document.createElement('div')
 
-        name.classList.add('item__name')
+        repoLink.classList.add('item__link')
         description.classList.add('item__description')
         owner.classList.add('item__owner')
+        stars.classList.add('item__stars')
 
+        repoLink.setAttribute('href', elem.html_url)
+        repoLink.setAttribute('target', '_blank')
+        repoLink.textContent = elem.name
 
-        name.textContent = elem.full_name
-        description.textContent = elem.description
-        owner.textContent = elem.owner.login
+        description.textContent = elem.description ? 'Описание: ' + elem.description : 'No description.'
+        owner.textContent = 'Автор: ' + elem.owner.login
+        // Небольшое форматирование количества лайков - показывать тысячи звездочек.
+        stars.textContent = '☆ ' + ((elem.stargazers_count > 1000) ?
+        (elem.stargazers_count / 1000).toFixed(1) + 'k' :
+        elem.stargazers_count) 
 
-        li.append(name)
+        li.append(repoLink)
         li.append(description)
         li.append(owner)
+        li.append(stars)
 
         resultList.append(li)
     }
@@ -82,8 +93,23 @@ function clearResultList() {
     }
 }
 
+function showLoadingMessage() {
+    let resultList = document.querySelector('.result')
+    let loadingMessage = document.createElement('li')
+    loadingMessage.classList.add('result__loading')
+    loadingMessage.textContent = 'Загрузка...'
+    resultList.append(loadingMessage)
+}
+
+function hideLoadingMessage() {
+    let loadingMessage = document.querySelector('.result__loading')
+    loadingMessage.innerHTML = ''
+}
+
 let searchInput = document.querySelector('.search__input')
 let searchButton = document.querySelector('.search__button')
 
 handleKeypress(searchInput)
 handleClick(searchButton)
+
+// Кэширование?
